293889368e5fa4726a0f74c44ef946fa525e4aba
Themba  Ntimane <141935793+222176415@users.noreply.github.com>
2024-11-18 20:56:49 +0000

    Challenge

--- src/challenge.js
+++ src/challenge.js
@@ -5,6 +5,15 @@ export default function counterApp() {
   const [date, setDate] = useState(new Date());
   const [step, setStep] = useState(0);
   const [counter, setCounter] = useState(0);
+
+  const HandleClick = () => {
+    setCounter((s) => s + 1);
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + 1);
+    setDate(newDate);
+  };
+
+  const incrementDate = () => {};
   return (
     <div>
       <main className="countercard">
@@ -20,11 +29,11 @@ export default function counterApp() {
           -
         </button>
         <p>Count : {counter} </p>
-        <button className="btn" onClick={() => setCounter((s) => s + 1)}>
+        <button className="btn" onClick={HandleClick}>
           +
         </button>
       </main>
-      <p>{date.toDateString()}</p>
+      <p>Today is {date.toDateString()}</p>
     </div>
   );
 }
